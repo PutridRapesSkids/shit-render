1b39e2dafeaa8fe7f919776c0ead65faf1467840
putrid#0001 <43300931+PutridRapesSkids@users.noreply.github.com>
2022-05-15 17:31:53 -0400

    Add files via upload

---
+++ dogshitrender.lua
@@ -0,0 +1,47 @@
+
+-- Instances:
+local render = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local Frame = Instance.new("Frame")
+local RenderMadeBygrim0001 = Instance.new("TextLabel")
+local grimsrenderscript = Instance.new("TextLabel")
+
+--Properties:
+render.Name = "render"
+render.Parent = game.CoreGui
+render.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Main.Name = "Main"
+Main.Parent = render
+Main.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+Main.BorderSizePixel = 0
+Main.Position = UDim2.new(-0.0369580649, 0, -0.0177383646, 0)
+Main.Size = UDim2.new(1.08799398, 0, 1.06143343, 0)
+
+Frame.Parent = Main
+Frame.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.317576081, 0, 0.257912278, 0)
+Frame.Size = UDim2.new(0.490747124, 0, 0.439442664, 0)
+
+RenderMadeBygrim0001.Name = "Render Maked By \"឵grim#0001\""
+RenderMadeBygrim0001.Parent = Frame
+RenderMadeBygrim0001.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+RenderMadeBygrim0001.BorderSizePixel = 0
+RenderMadeBygrim0001.Position = UDim2.new(0.189944118, 0, -0.0219512247, 0)
+RenderMadeBygrim0001.Size = UDim2.new(0, 418, 0, 65)
+RenderMadeBygrim0001.Font = Enum.Font.Code
+RenderMadeBygrim0001.Text = "Render Maked By \"឵grim#0001\""
+RenderMadeBygrim0001.TextColor3 = Color3.fromRGB(0, 0, 0)
+RenderMadeBygrim0001.TextSize = 200.000
+
+grimsrenderscript.Name = "grim's render script"
+grimsrenderscript.Parent = Frame
+grimsrenderscript.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+grimsrenderscript.BorderSizePixel = 0
+grimsrenderscript.Position = UDim2.new(0.189944118, 0, 0.20243901, 0)
+grimsrenderscript.Size = UDim2.new(0, 418, 0, 65)
+grimsrenderscript.Font = Enum.Font.Code
+grimsrenderscript.Text = "grim's render script"
+grimsrenderscript.TextColor3 = Color3.fromRGB(0, 0, 0)
+grimsrenderscript.TextSize = 200.000
